f66efe6818b0a28f59bb9ab4cefc5b2637b31d9b
Oluwasegun Ajayi <ajayimooseize@gmail.com>
2021-06-22 22:12:28 +0100

    test 2

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -43,4 +43,40 @@ suite("Functional Tests", function () {
         done();
       });
   });
+
+  test("Create an issue with every field: POST request to /api/issues/{project}", function (done) {
+    let newIssue = {
+      assigned_to: "John Doe",
+      status_text: "New Issue, Treat as urgent",
+      issue_title: "First test with all fields required",
+      issue_text: "Test 1",
+      created_by: "Sakarious",
+    };
+    chai
+      .request(server)
+      .post("/api/issues/apitest")
+      .send(newIssue)
+      .end(function (err, res) {
+        //console.log(res.body);
+        assert.equal(res.status, 200);
+        res.body.should.be.a("object");
+        res.body.should.have.property("_id");
+        res.body.should.have.property("open");
+        res.body.should.have.property("issue_title");
+        res.body.should.have.property("issue_text");
+        res.body.should.have.property("created_by");
+        res.body.should.have.property("assigned_to");
+        res.body.should.have.property("status_text");
+        res.body.should.have.property("created_on");
+        res.body.should.have.property("updated_on");
+        assert.equal(res.body.open, true);
+        assert.equal(
+          res.body.issue_title,
+          "First test with all fields required"
+        );
+        assert.equal(res.body.issue_text, "Test 1");
+        assert.equal(res.body.created_by, "Sakarious");
+        done();
+      });
+  });
 });
