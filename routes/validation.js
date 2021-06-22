cb75ed00b87b50c457464ef271f719bd9c9dbdcb
Oluwasegun Ajayi <ajayimooseize@gmail.com>
2021-06-22 23:11:09 +0100

    test 3

--- routes/validation.js
+++ routes/validation.js
@@ -3,6 +3,14 @@ module.exports = class Validation {
     let error = {};
 
     if (
+      typeof title == undefined ||
+      typeof title == undefined ||
+      typeof createdBy == undefined
+    ) {
+      error.description = "title, text or created_by fields cannot be empty";
+    }
+
+    if (
       title.trim().length == 0 ||
       text.trim().length == 0 ||
       createdBy.trim().length == 0
